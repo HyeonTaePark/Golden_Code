6fc0d121c046a735d54f013c7ef8a60b557e4415
HyeonTaePark <98525916+HyeonTaePark@users.noreply.github.com>
2022-01-28 10:22:42 +0900

    Update Auto_trade.py

--- Auto_trade.py
+++ Auto_trade.py
@@ -2,8 +2,8 @@ import time
 import pyupbit
 import datetime
 
-access = "mpaED5oiKQDkhIIvweKOTH9WsnbIjywou7MCTCec"
-secret = "veqYfo2ZVNFxYYex0hS5nsYXpGRspxnmyqpPfUCq"
+access = ""
+secret = ""
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
